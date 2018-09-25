cd0eea3fd073f75a736011f4665ea2291885596b
Victor Buzzegoli <vbuzze@gmail.com>
2018-09-25 12:16:43 -0500

    Updating middleware

--- itunes-explorer/src/store/index.js
+++ itunes-explorer/src/store/index.js
@@ -2,9 +2,8 @@ import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "./reducers";
 
 import thunk from "redux-thunk";
-import axiom from "../mw/axiom";
 
-const middleware = [thunk, axiom];
+const middleware = [thunk];
 
 const withDevTools = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
