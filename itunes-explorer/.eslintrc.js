2b1cf3979ec45eb12ad0c2b1f567c700cddfe13f
Victor Buzzegoli <vbuzze@gmail.com>
2018-09-25 11:47:59 -0500

    Adding linting config

---
+++ itunes-explorer/.eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+	env: {
+		browser: true,
+		es6: true
+	},
+	extends: "eslint:recommended",
+	parser: "babel-eslint",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 2018,
+		sourceType: "module"
+	},
+	plugins: ["react"],
+	rules: {
+		indent: ["off", "tab"],
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+		"no-console": 0,
+		"no-undef": 1,
+		"no-unused-vars": [
+			"warn",
+			{ vars: "all", args: "after-used", ignoreRestSiblings: false }
+		],
+		"react/jsx-filename-extension": ["warn", { extensions: [".jsx"] }]
+	}
+};
